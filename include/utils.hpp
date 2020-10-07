111e0775ad75cfc11c3a0c14f648a083f65fd01d
zanazakaryaie <zana.zakaryaie@gmail.com>
2020-10-07 13:35:49 +0330

    utilities for file/folder manipulation

---
+++ include/utils.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/utils/filesystem.hpp>
+
+
+bool doesExist(const std::string& dirName); //Checks if directory exists
+
+void createDirectory(const std::string& dirName); //Creates directory if it does not exist
+
+void removeDirectory(const std::string& dirName); //Removes directory if it exists
+
+std::vector<cv::String> listFiles(const std::string& dirName, const std::vector<std::string>& extensions, bool relative=true); //Lists all files in dirName with extensions.
+
+std::string selectFile(const std::string& title, const std::vector<std::string>& extensions); //Opens dialog box for the user to select files with specified extensions
+
+std::string selectFolder(const std::string& title); //Opens dialog box for the user to select a folder
+
+std::string currentDateTime(void); //Returns current date and time. Used for naming samples uniquely
+
+
