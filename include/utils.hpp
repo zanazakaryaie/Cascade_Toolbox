0616c3d55c1e07f2d22055e477d7a179b7cb0909
zanazakaryaie <zana.zakaryaie@gmail.com>
2021-06-19 18:30:08 +0430

    removed dependency to boost, moved unnecessary header files to src/utils.cpp

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,10 +1,7 @@
 #pragma once
 
-#include <iostream>
-#include <fstream>
-#include <opencv2/opencv.hpp>
-#include <opencv2/core/utils/filesystem.hpp>
-
+#include <vector>
+#include <string>
 
 bool doesExist(const std::string& dirName); //Checks if directory exists
 
@@ -12,7 +9,7 @@ void createDirectory(const std::string& dirName); //Creates directory if it does
 
 void removeDirectory(const std::string& dirName); //Removes directory if it exists
 
-std::vector<cv::String> listFiles(const std::string& dirName, const std::vector<std::string>& extensions, bool relative=true); //Lists all files in dirName with extensions.
+std::vector<std::string> listFiles(const std::string& dirName, const std::vector<std::string>& extensions, bool relative=true); //Lists all files in dirName with extensions.
 
 std::string selectFile(const std::string& title, const std::vector<std::string>& extensions); //Opens dialog box for the user to select files with specified extensions
 
